6b656cd1c9aad0817a23ea9d9850c5c3be67ec88
petrpajdla <petr.pajdla@protonmail.com>
2023-07-24 15:10:49 +0200

    reorder scripts

--- code/02_create_meta.R
+++ code/02_create_meta.R
@@ -8,8 +8,6 @@ library(googlesheets4)
 # paths -------------------------------------------------------------------
 
 dir_data <- here::here("data")
-# dir_raw <- paste0(dir_data, "/raw")
-# dir_der <- paste0(dir_data, "/derived")
 dir_fin <- paste0(dir_data, "/final")
 
 
@@ -59,8 +57,6 @@ oao_gd <- read_sheet(revised_gd_url, sheet = "oao_webapp") %>%
 
 
 # geocode address ---------------------------------------------------------
-
-# not implemented in the webapp yet
 
 address <- oao_gd %>%
   pull(adresa) %>%
